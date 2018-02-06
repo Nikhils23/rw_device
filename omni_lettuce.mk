c154a6df14e18c846e1dde6dc3019496e595eab2
Ad!thya R <34226885+ghostrider-reborn@users.noreply.github.com>
2018-02-06 10:05:56 +0530

    Changes for RW

--- omni_lettuce.mk
+++ omni_lettuce.mk
@@ -1,8 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := lettuce
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -15,6 +15,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dt.img:dt.img \
     $(LOCAL_PATH)/kernel:kernel \
     $(LOCAL_PATH)/recovery.fstab:root/etc/recovery.fstab
+
+# Time Zone data for recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lettuce
